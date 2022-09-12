6601e6653cb6d94251052d77ce8d4647054180a4
“awisniewski” <adam.wisniewski.it@gmail.com>
2022-09-12 14:54:06 +0200

    mobile rotation fix

--- Assets/Scripts/MobileRotation.cs
+++ Assets/Scripts/MobileRotation.cs
@@ -22,7 +22,13 @@ public class MobileRotation : MonoBehaviour
         }
 
         Vector3 v = new Vector3(0, angle, 0);
-        player.transform.rotation = Quaternion.Euler(v);
+
+        Vector2 direction = new Vector2(joystick.Horizontal, joystick.Vertical);
+        if (direction.sqrMagnitude > 0.1f)
+        {
+            player.transform.rotation = Quaternion.Euler(v);
+            player.transform.eulerAngles = new Vector3(player.transform.eulerAngles.x, angle, player.transform.eulerAngles.z);
+        }
     }
 
 }
